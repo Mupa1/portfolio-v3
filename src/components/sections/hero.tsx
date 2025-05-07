fabaeca48709627c68e0aa6c3897445db2b18bdc
Mupa1 <mupasmail@gmail.com>
2025-05-07 13:18:58 +0200

    Move overlay to layout

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,25 +1,20 @@
 import { useTranslations } from "next-intl";
 import React from "react";
 
-import Overlay from "../ui/overlay";
-
 const Hero = () => {
   const t = useTranslations();
 
   return (
-    <>
-      <Overlay />
-      <section
-        id="home"
-        className="flex-center h-screen w-full snap-start snap-always flex-col gap-3 text-center"
-      >
-        <h2>
-          <div>{t("Hero.frontend")}</div>
-          <div>{t("Hero.engineer")}</div>
-        </h2>
-        <p className="">{t("Hero.description")}</p>
-      </section>
-    </>
+    <section
+      id="home"
+      className="flex-center h-screen w-full snap-start snap-always flex-col gap-3 text-center"
+    >
+      <h2>
+        <div>{t("Hero.frontend")}</div>
+        <div>{t("Hero.engineer")}</div>
+      </h2>
+      <p className="">{t("Hero.description")}</p>
+    </section>
   );
 };
 

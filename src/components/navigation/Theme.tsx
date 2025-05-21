7ef728c044c6e6345ac05fe0776130866e94af68
Mupa1 <mupasmail@gmail.com>
2025-05-21 13:58:23 +0200

    Improve styles

--- src/components/navigation/Theme.tsx
+++ src/components/navigation/Theme.tsx
@@ -19,7 +19,10 @@ const Theme = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="hover:animate-wiggle">
+        <Button
+          variant="ghost"
+          className="hover:animate-wiggle focus:outline-none"
+        >
           <Sun className="rotate-0 scale-100 text-neutral-900 transition-all dark:-rotate-90 dark:scale-0 dark:text-neutral-100" />
           <MoonIcon className="absolute rotate-90 scale-0 text-neutral-900 transition-all dark:rotate-0 dark:scale-100 dark:text-neutral-100" />
           <span className="sr-only">Toggle theme</span>

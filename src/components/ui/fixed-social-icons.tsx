f361af09358797dbd22da0544ba6739e95af1643
Mupa1 <mupasmail@gmail.com>
2026-01-16 12:30:12 +0100

    add glow effects and improve styling for social icons

--- src/components/ui/fixed-social-icons.tsx
+++ src/components/ui/fixed-social-icons.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 import { useSocialIconsVisibility } from "@/context/SocialIconsVisibility";
 import { cn } from "@/lib/utils";
 
@@ -7,16 +9,18 @@ import SocialIcons from "./social-icons";
 
 const FixedSocialIcons = () => {
   const { isFooterInView } = useSocialIconsVisibility();
+  const t = useTranslations("Accessibility");
 
   return (
-    <div
+    <aside
       className={cn(
         "fixed bottom-5 left-0 z-10 hidden h-screen w-14 flex-col items-center justify-end transition-opacity duration-300 md:left-7 md:flex",
         isFooterInView ? "opacity-0" : "opacity-100"
       )}
+      aria-label={t("fixedSocialLinks")}
     >
       <SocialIcons className="flex flex-col gap-6 text-lg font-semibold leading-6" />
-    </div>
+    </aside>
   );
 };
 
